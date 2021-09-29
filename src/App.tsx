5401f94028338d5ad26ccd189916f98b41c6bdc1
David Kalina <kalinadavid404@gmail.com>
2021-09-29 10:56:42 -0600

    Created a socket context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { ChakraProvider, theme } from '@chakra-ui/react'
 import * as React from 'react'
-import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme } from '@chakra-ui/react'
-import { ColorModeSwitcher } from './ColorModeSwitcher'
-import { Logo } from './Logo'
 
-export const App = () => <ChakraProvider theme={theme}>Build something nuts</ChakraProvider>
+export const App = () => {
+  return <ChakraProvider theme={theme}></ChakraProvider>
+}
