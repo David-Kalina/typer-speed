8353ae0aa911491f142a378ca9375711eb2dba46
David Kalina <kalinadavid404@gmail.com>
2021-10-03 23:36:32 -0600

    Default dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, ChakraProvider, theme, VStack } from '@chakra-ui/react'
+import { Box, ChakraProvider, extendTheme, VStack } from '@chakra-ui/react'
 import * as React from 'react'
 import KeyHandler from './components/KeyHandler'
 import NewTest from './components/NewTest'
@@ -7,6 +7,13 @@ import Timer from './components/Timer'
 import WordsDisplay from './components/WordsDisplay'
 import WordsPerMinute from './components/WordsPerMinute'
 import { socket, SocketContext } from './contexts/SocketContext'
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+// 3. extend the theme
+const theme = extendTheme({ config } as any)
 
 export const App = () => {
   return (
