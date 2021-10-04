a674d3f107c8bac004057b459a7cf3af1d41b455
David Kalina <kalinadavid404@gmail.com>
2021-10-03 23:00:27 -0600

    Added env var

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { io, Socket } from 'socket.io-client'
 import { DefaultEventsMap } from 'socket.io-client/build/typed-events'
 
-export const socket = io('http://localhost:4000')
+export const socket = io(process.env.REACT_APP_SOCKET_SERVER as string)
 export const SocketContext = React.createContext<Socket<DefaultEventsMap, DefaultEventsMap> | null>(
   null
 )
