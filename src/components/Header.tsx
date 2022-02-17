8dbc7037739971a2da2a993b6477193a7580a890
David Kalina <kalinadavid404@gmail.com>
2022-02-17 14:18:08 -0700

    remove unused header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Flex, HStack, Text, useBreakpoint } from '@chakra-ui/react'
 import { useAtom } from 'jotai'
 import React from 'react'
-import { FiLogOut, FiUser } from 'react-icons/fi'
+import { FiLogOut } from 'react-icons/fi'
 import { Link, useLocation } from 'react-location'
 import { useAuth } from '../contexts/AuthContext'
 import { themeAtom } from '../store/typingTestAtoms'
@@ -26,14 +26,14 @@ function Header() {
         <HStack spacing={6}>
           <Logo />
           <Link to="/"></Link>
-          <Link to={user?.email ? '/account' : '/login'}>
+          {/* <Link to={user?.email ? '/account' : '/login'}>
             <Flex align="center" cursor="pointer" pos="relative">
               <FiUser />
               <Text ml="4px" fontSize="xs">
                 {user?.email} {!user?.email ? 'sign in' : null}
               </Text>
             </Flex>
-          </Link>
+          </Link> */}
         </HStack>
         <HStack spacing={4}>
           <SetTestTime />
