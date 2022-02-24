7527b7f7a8003f0d117804e8d17e8a98c2742c78
David Kalina <kalinadavid404@gmail.com>
2022-02-23 22:47:09 -0700

    key command

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, Link, Text, useBreakpoint, VStack } from '@chakra-ui/react'
+import { Flex, HStack, Kbd, Link, Text, useBreakpoint, VStack } from '@chakra-ui/react'
 import { useAtom } from 'jotai'
 import React from 'react'
 import { IoLogoGithub } from 'react-icons/io'
@@ -10,7 +10,7 @@ function Footer() {
 
   if (breakpoint === 'md' || breakpoint === 'lg' || breakpoint === 'xl' || breakpoint === '2xl') {
     return (
-      <HStack w="100%" p="12" justifyContent="center" fontSize="sm" color={theme.textLight}>
+      <HStack w="100%" spacing={12} p="12" justifyContent="center" fontSize="sm" color={theme.textLight}>
         <Link
           cursor="pointer"
           href="https://github.com/David-Kalina/typing-speed-test-with-socket.io-client"
@@ -21,6 +21,16 @@ function Footer() {
             <Text ml="4px">github</Text>
           </Flex>
         </Link>
+        <Flex align="center">
+          <Kbd mx="4px" bg={theme.textDark}>
+            tab
+          </Kbd>{' '}
+          +{' '}
+          <Kbd mx="4px" bg={theme.textDark}>
+            enter
+          </Kbd>{' '}
+          to start over
+        </Flex>
       </HStack>
     )
   } else {
